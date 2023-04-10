78583f5b084d9f9df3c69a0db93f7b4d4c7bd217
Kelsie Szost <kelsieszost@Kelsies-Air.localdomain>
2023-04-10 15:53:37 -0400

    cart model

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-cart',
-  template: `
-    <p>
-      cart works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: "./cart.component.html"
 })
 export class CartComponent implements OnInit {
+  cart: Cart = {items: []};
 
   constructor() { }
 
